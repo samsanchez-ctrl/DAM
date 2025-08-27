bfabd5e576212c248d4653dd9e288a752cfd780e
samsanchez-ctrl <sam.sanchez@duocuc.cl>
2025-08-27 12:38:34 -0400

    mis tres commit and push

--- Clase1.kt
+++ Clase1.kt
@@ -1,8 +1,9 @@
 
 
 fun main(){
-    for(i in 1 .. 10){
-        println("Vamos en el valor $i")
+    for(i in 1..20){
+        if(i == 5) continue
+        if(i in 11..19) continue
+        println("vamos en el valor $i")
     }
-
 }
---
+++ Clase2.kt
@@ -0,0 +1,8 @@
+fun main(){
+    for(i in 1..30){
+        if(i % 2 == 0) {
+            println("Vamos en el valor $i")
+        }
+    }
+}
+
